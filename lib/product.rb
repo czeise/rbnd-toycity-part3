e467eb3158f134094fd9ae7f557b8cc5af07a45d
Craig Zeise <craig.zeise@gmail.com>
2016-01-11 22:14:24 -0600

    Adds product methods for first part of project.

--- lib/product.rb
+++ lib/product.rb
@@ -2,12 +2,14 @@ require_relative 'errors'
 
 # Product class
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options = {})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
@@ -15,11 +17,31 @@ class Product
     @@products
   end
 
+  def self.find_by_title(title)
+    @@products.each do |product|
+      return product if product.title == title
+    end
+  end
+
+  def in_stock?
+    @stock > 0 ? true : false
+  end
+
+  def self.in_stock
+    products_in_stock = []
+    @@products.each do |product|
+      if product.stock > 0
+        products_in_stock << product
+      end
+    end
+    products_in_stock
+  end
+
   private
 
   def add_to_products
-    @@products.each do |item|
-      raise DuplicateProductError if item.title == @title
+    @@products.each do |product|
+      raise DuplicateProductError if product.title == @title
     end
 
     @@products << self
