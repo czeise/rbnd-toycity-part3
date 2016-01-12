6c87df3eced6d3af9dceff9893f1c234905b0f25
Craig Zeise <craig.zeise@gmail.com>
2016-01-11 21:48:30 -0600

    Adds error if duplicate product is detected.

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,27 @@
+require_relative 'errors'
+
 # Product class
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products.each do |item|
+      raise DuplicateProductError if item.title == @title
+    end
+
+    @@products << self
   end
 end
